094cdb7e616110b955e213ecb046f05908d7134d
hagar575 <132882286+hagar575@users.noreply.github.com>
2025-06-20 15:45:55 +0300

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+import requests # Import the requests library to handle HTTP requests
+from bs4 import BeautifulSoup # Import BeautifulSoup from bs4 to parse HTML content
 
-url = 'https://edition.cnn.com/world'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
+url = 'https://edition.cnn.com/world' # URL of the CNN World section
+response = requests.get(url) # Send a GET request to the URL
+soup = BeautifulSoup(response.text, 'lxml') # Parse the HTML content using BeautifulSoup
 
-for title in soup.find_all('span', class_='container__headline-text'):
-    print(title.text.strip())
+for title in soup.find_all('span', class_='container__headline-text'): # Find all span elements with the specified class
+    print(title.text.strip())  # Print the text content of each title, stripping any extra whitespace
+    print('---')
